81dd9108b11f9f905c19df7d10f0e880cfc5b1eb
vuluu2k <vuluu040320@gmail.com>
2024-12-21 00:46:36 +0700

    add: package you need

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+
 const app = express();
 
 // init middleware
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
 
 // init database
+
+// init router
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello world",
+  });
+});
 
 module.exports = app;
